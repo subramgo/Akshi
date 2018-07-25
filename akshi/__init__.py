dac2ef5cfff3184a0eac6bb71ea91922ed1c6f43
Gopi Subramanian <gsubramanian@tycoint.com>
2018-07-25 11:27:32 -0400

    modify init.py

--- akshi/__init__.py
+++ akshi/__init__.py
@@ -1,11 +1,7 @@
 from flask import Flask 
 from flask_bootstrap import Bootstrap 
-from akshi.gender.gender import gender_api
-from akshi.gender.models import gender_db,Gender
-from akshi.face.models import face_db, Face
-from akshi.face.face import face_api
-from akshi.object.object import object_api
-from akshi.object.models import object_db, Person
+from akshi.signage.signage import signage_api
+from akshi.signage.models import signage_db
 
 
 
@@ -20,22 +16,12 @@ photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 
 
-# Database
-gender_db.init_app(app)
-gender_db.create_all(app=app)
+signage_db.init_app(app)
+signage_db.create_all(app=app)
 
-face_db.init_app(app)
-face_db.create_all(app=app)
+# Signage
+app.register_blueprint(signage_api, url_prefix='/api/v1/signage')
 
-object_db.init_app(app)
-object_db.create_all(app=app)
-
-# Gender detection apis
-app.register_blueprint(gender_api, url_prefix='/api/v1/gender')
-# Face detection api
-app.register_blueprint(face_api, url_prefix='/api/v1/face')
-# object detection api
-app.register_blueprint(object_api, url_prefix='/api/v1/object')
 
 
 Bootstrap(app)
