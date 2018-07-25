f65ea1864a8a33b7579298675f76115aa95fa189
Gopi Subramanian <gsubramanian@tycoint.com>
2018-07-25 10:41:04 -0400

    cleaned up for signage project

---
+++ akshi/signage/signage.py
@@ -0,0 +1,35 @@
+from flask import render_template, flash, redirect, Blueprint, request, send_from_directory, Response
+import json
+import numpy as np 
+import cv2
+from .models import  signage_db,FaceSignage
+from sqlalchemy import func
+import sys
+import dlib
+from flask import current_app as app
+import os
+import time
+import json
+
+signage_api = Blueprint('signage', __name__, url_prefix='/api/v1/signage', template_folder = 'templates', static_folder = 'static')
+face_detector = dlib.get_frontal_face_detector()
+
+
+
+@signage_api.route('/', methods =['GET'])
+def signage_page():
+    signs = FaceSignage.query.order_by(FaceSignage.date_created.desc()).all()
+    signs_json = json.dumps([sign.get_json() for sign in signs])
+    return Response(signs_json, mimetype="application/json")
+ 
+
+@signage_api.route('/signage_upload', methods=['POST'])
+def signage_upload():
+    payload = request.json
+    signage_obj = FaceSignage(image_id = "epoch_time", no_faces = payload['no_faces'], windows = payload['windows'], camera_id = payload['camera_id'])
+    signage_db.session.add(signage_obj)
+    signage_db.session.commit()
+    return  Response(response={'status': 'SUCCESS'}, status=200, mimetype="application/json")
+
+
+
--- akshi/views.py
+++ akshi/views.py
@@ -1,6 +1,5 @@
 from flask import render_template, flash, redirect
 from akshi import  app
-from .forms import LoginForm
 
 
 @app.route('/')
@@ -10,11 +9,3 @@ def index():
 	return render_template('index.html', title='Akshi', user=user)
 
 
-@app.route('/login',methods=['GET', 'POST'])
-def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for OpenId ="%s", remember_me=%s' %
-			(form.openid.data, str(form.remember_me.data)))
-		return redirect('/index')
-	return render_template('login.html', title='sign in', form=form, providers=app.config['OPENID_PROVIDERS'])
